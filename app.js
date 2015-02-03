36ed03ab82b331c8e33e552312738f994c8fff8a
David Mihal <david.mihal@endicia.com>
2015-02-03 13:59:28 -0800

    Simple UI to read files

---
+++ app.js
@@ -0,0 +1,29 @@
+function handleFileSelect(evt) {
+  var files = evt.target.files; // FileList object
+
+  output = [];
+  // Loop through the FileList and render image files as thumbnails.
+  for (var i = 0, f; f = files[i]; i++) {
+    output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
+                  f.size, ' bytes, last modified: ',
+                  f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
+                  '</li>');
+    var reader = new FileReader();
+    reader.readAsText(f);
+    reader.onloadend = function(){
+      readXML(reader.result);
+    };
+  }
+  document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
+
+  // Read in the image file as a data URL.
+  //reader.readAsDataURL(f);
+};
+function readXML(xmlStr){
+  var parser = new window.DOMParser();
+
+  var xml = parser.parseFromString(xmlStr, "text/xml");
+  console.log(xml);
+};
+
+document.getElementById('files').addEventListener('change', handleFileSelect, false);
