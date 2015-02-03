b1fc075ed48a3313ff748e1f6fe0650c2470e073
David Mihal <eastsideski@gmail.com>
2015-02-03 23:28:43 +0000

    Parse points

--- workers/fileLoader.js
+++ workers/fileLoader.js
@@ -1,8 +1,19 @@
 FileLoader = (function(){
   var reader = new FileReaderSync();
   
-  function readPoints(xml){
-    return [{date: 0, lat: 0, lng: 0}];
+  var re = /<when>([0-9\-T:\.]+)<\/when>\n<gx:coord>(-?\d*(?:.\d+)?) (-?\d*(?:.\d+)?) 0<\/gx:coord>/g;
+  
+  function readPoints(xmlStr){
+    var out = []
+    while (match = re.exec(xmlStr)){
+      var pnt = {
+        date: new Date(match[1]),
+        lat: Number.parseFloat(match[2]),
+        lng: Number.parseFloat(match[3])
+      }
+      out.push(pnt);
+    }
+    return out;
   }
   
   return function(file){
