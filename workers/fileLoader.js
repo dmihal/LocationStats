df836c789a54119ea2188ff6c9fe412f586afc0d
David Mihal <eastsideski@gmail.com>
2015-02-04 19:19:44 +0000

    Track progress and performance of fileLoader

--- workers/fileLoader.js
+++ workers/fileLoader.js
@@ -4,7 +4,8 @@ FileLoader = (function(){
   var re = /<when>([0-9\-T:\.]+)<\/when>\n<gx:coord>(-?\d*(?:.\d+)?) (-?\d*(?:.\d+)?) 0<\/gx:coord>/g;
   
   function readPoints(xmlStr){
-    var out = []
+    Performance.start('FileParser');
+    var out = [];
     while (match = re.exec(xmlStr)){
       var pnt = {
         date: new Date(match[1]),
@@ -12,12 +13,22 @@ FileLoader = (function(){
         lng: Number.parseFloat(match[3])
       }
       out.push(pnt);
+
+      self.postMessage({
+        cmd: "progress",
+        task: "Reading DataPoints",
+        val: match.index,
+        max: xmlStr.length
+      });
     }
+    Performance.stop('FileParser');
     return out;
   }
   
   return function(file){
+    Performance.start('FileReader');
     var xmlStr = reader.readAsText(file);
+    Performance.stop('FileReader');
     var points = readPoints(xmlStr);
     return points;
   };
